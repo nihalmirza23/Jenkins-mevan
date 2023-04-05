c6f3bf033bf6cfb3b784a00827fff3d0a330fd7b
Gaurav <gauravlavand0102@gmail.com>
2023-04-05 19:41:49 +0530

    maven project

---
+++ Jenkins-maven/Abc/src/co/in/Try.java
@@ -0,0 +1,8 @@
+package co.in;
+
+public class Try {
+	
+	public static void main(String[] args) {
+		System.out.println("great idea");
+	}
+}
---
+++ Jenkins-maven/Abc/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author GGANPATL
+ *
+ */
+module Abc {
+}
